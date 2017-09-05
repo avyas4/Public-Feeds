f581308003828c057bb91bf16fc1372be1bdd26d
Astha Vyas <vyas.astha293@gmail.com>
2017-09-05 22:21:12 +1000

    Add bower-component

--- app/feeds/controller/index.js
+++ app/feeds/controller/index.js
@@ -2,14 +2,13 @@
     'use strict';
     // Declare app level module which depends on views, and services
     angular.module('myApp', ['feedsService'])
-        .controller("feedsController", ['$scope', 'getFeedsData', '$q', function($scope, getFeedsData, $q) {
+        .controller("feedsController", ['$scope', 'getFeedsData', function($scope, getFeedsData) {
             $scope.layout = 'grid';
 
             /*  getFeedsData is an instance of a service 
                 This function will get data from services  
              */
 
-            var deferred = $q.defer();
             getFeedsData.getData(function(response) {
                 $scope.records = response.feed.entry;
             }, function(error) {
